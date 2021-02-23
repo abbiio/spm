aae08ef69921b2c56bf399017ed3a742d03ed3c7
Sean-Kone <Sean-Kone@users.noreply.github.com>
2021-02-23 13:05:24 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-​
+
 import PackageDescription
-​
+
 let package = Package(name: "WalkMeSDK",
                       products: [.library(name: "WalkMeSDK",
                                           targets: ["WalkMeSDK"])],
