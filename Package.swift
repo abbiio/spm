e5bf0c43cc43a95f5f8bbaa52486321fd73af630
Or Elmaliah <orxelm@users.noreply.github.com>
2021-02-23 11:53:25 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,16 +3,10 @@
 ​
 import PackageDescription
 ​
-let package = Package(
-    name: "WalkMeSDK",
-    products: [
-        .library(
-                name: "WalkMeSDK",
-                targets: ["WalkMeSDK"])
-    ],
-    targets: [
-        .binaryTarget(name: "WalkMeSDK",
-                      url: "https://wmm-assets-prod.s3.amazonaws.com/release/sdk/2.9.12.xcframework.zip",
-                      checksum: "1a49f99b0b65f6f109b106560f98b1f89614c33c9dd337b9821b621d943fc380")
-    ]
-)
+let package = Package(name: "WalkMeSDK",
+                      products: [.library(name: "WalkMeSDK",
+                                          targets: ["WalkMeSDK"])],
+                      targets: [.binaryTarget(name: "WalkMeSDK",
+                                              url: "https://wmm-assets-prod.s3.amazonaws.com/release/sdk/2.9.12.xcframework.zip",
+                                              checksum: "1a49f99b0b65f6f109b106560f98b1f89614c33c9dd337b9821b621d943fc380")],
+                      swiftLanguageVersions: [.v5])
