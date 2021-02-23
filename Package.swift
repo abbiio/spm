5ffc719bbeb46a5d579b0166867ada4c197b96b3
Or Elmaliah <orxelm@users.noreply.github.com>
2021-02-23 10:48:27 +0200

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+​
+import PackageDescription
+​
+let package = Package(
+    name: "WalkMeSDK",
+    products: [
+        .library(
+                name: "WalkMeSDK",
+                targets: ["WalkMeSDK"])
+    ],
+    targets: [
+        .binaryTarget(name: "WalkMeSDK",
+                      url: "https://wmm-assets-prod.s3.amazonaws.com/release/sdk/2.9.12.xcframework.zip",
+                      checksum: "1a49f99b0b65f6f109b106560f98b1f89614c33c9dd337b9821b621d943fc380")
+    ]
