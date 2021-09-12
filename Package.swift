50bf96fbdc6074e44e25a0209c4e2d06f33d879d
Sean-Kone <Sean-Kone@users.noreply.github.com>
2021-09-12 14:20:17 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,12 @@ import PackageDescription
 
 let package = Package(
   name: "WalkMeSDK",
-  products: [.library(name: "WalkMeSDK",
-                      targets: ["WalkMeSDK"])],
-  targets: [.binaryTarget(name: "WalkMeSDK",
-                          url: "https://assets.abbi.io/release/sdk/2.10.3.xcframework.zip",
-                          checksum: "96f8586c87c196063602605fe12772c9ae802b14bff9e925c7d6e17bbfe2fc8a")],
-  swiftLanguageVersions: [.v5]
+  products: [
+    .library(name: "WalkMeSDK", targets: ["WalkMeSDK"])
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .binaryTarget(name: "WalkMeSDK", path: "WalkMeSDK.xcframework")
+  ]
 )
